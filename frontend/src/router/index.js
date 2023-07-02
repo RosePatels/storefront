4b1891a2880d9491266bef7c69e8b9d8bef68178
Shree Patel <shreepateldev@gmail.com>
2023-07-02 12:39:24 -0400

    Create 404 not found page

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import ProductList from "../components/ProductList.vue";
 import ProductForm from "../components/ProductForm.vue";
 import ProductDetail from "../components/ProductDetail.vue";
+import NotFound from "../components/NotFound.vue";
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
@@ -21,6 +22,15 @@ const router = createRouter({
         name: 'product-detail',
         component: ProductDetail,
         props: true
+    },
+    {
+        path: '/home',
+        redirect: '/'
+    },
+    {
+        path: '/:catchAll(.*)',
+        name: 'not-found',
+        component: NotFound
     }
   ],
 })
